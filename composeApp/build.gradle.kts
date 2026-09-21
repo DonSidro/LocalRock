20df4f5e4a87da6b9f8c1fad4007ded144c95fb9
Donsidro <sidonkk@gmail.com>
2026-09-21 17:42:10 +0200

    Bump version to 1.1.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -85,8 +85,8 @@ android {
         applicationId = "com.kodraliu.localrock"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
     packaging {
         resources {
